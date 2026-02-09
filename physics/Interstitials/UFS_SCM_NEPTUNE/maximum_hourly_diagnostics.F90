4c1c13b3777464439c6fbb2f0529f2c1ef82b21f
benwgreen <54944233+benwgreen@users.noreply.github.com>
2026-02-09 07:50:34 -0700

    Update maximum_hourly_diagnostics.F90
    Adding hooks for TEMPO

--- physics/Interstitials/UFS_SCM_NEPTUNE/maximum_hourly_diagnostics.F90
+++ physics/Interstitials/UFS_SCM_NEPTUNE/maximum_hourly_diagnostics.F90
@@ -27,6 +27,7 @@ contains
 #endif
    subroutine maximum_hourly_diagnostics_run(im, levs, reset, lradar, imp_physics,                 &
                                              imp_physics_gfdl, imp_physics_thompson,               &
+                                             imp_physics_tempo,                                    &
                                              imp_physics_fer_hires, imp_physics_nssl,              &
                                              con_g, phil,                                          &
                                              gt0, refl_10cm, refdmax, refdmax263k, u10m, v10m,     &
@@ -40,7 +41,7 @@ contains
        integer, intent(in) :: im, levs, kdt
        logical, intent(in) :: reset, lradar, lightning_threat
        integer, intent(in) :: imp_physics, imp_physics_gfdl, imp_physics_thompson, imp_physics_fer_hires, &
-                              imp_physics_nssl
+                              imp_physics_nssl, imp_physics_tempo
        real(kind_phys), intent(in   ) :: con_g
        real(kind_phys), intent(in   ) :: con_rd
        real(kind_phys), intent(in   ) :: phil(:,:)
@@ -89,6 +90,7 @@ contains
 !Calculate hourly max 1-km agl and -10C reflectivity
        if (lradar .and. (imp_physics == imp_physics_gfdl .or. &
           imp_physics == imp_physics_thompson .or.    &
+          imp_physics == imp_physics_tempo    .or.    &
           imp_physics == imp_physics_fer_hires .or.   &
           imp_physics == imp_physics_nssl )) then
           allocate(refd(im))
